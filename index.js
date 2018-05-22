61dcf750848aba74af6be9bc67007ed3e3849e54
werowe <werowe@walkerrowe.com>
2018-05-22 14:23:39 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+const recipe = require('./triggers/qualtrics');
+
+const addAuthHeader = (request, z, bundle) => {
+  // Hard-coded auth header just for demo
+  request.headers['X-API-Key'] = 'secret';
+  return request;
+};
+
+// Now we can roll up all our behaviors in an App.
+const App = {
+  // This is just shorthand to reference the installed dependencies you have. Zapier will
+  // need to know these before we can upload
+  version: require('./package.json').version,
+  platformVersion: require('zapier-platform-core').version,
+
+  beforeRequest: [addAuthHeader],
+
+  afterResponse: [],
+
+  resources: {},
+
+  // If you want your trigger to show up, you better include it here!
+  triggers: {
+    [qualtrics.key]: listsurveys
+  },
+
+  // If you want your searches to show up, you better include it here!
+  searches: {},
+
+  // If you want your creates to show up, you better include it here!
+  creates: {}
+};
+
+// Finally, export the app.
+module.exports = App;
