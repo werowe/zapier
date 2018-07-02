1fa947c38908b6886cc38990ac78edb6291a5f35
werowe <werowe@walkerrowe.com>
2018-07-02 15:17:51 +0200

    Update and rename qualtrics.js to dropdown.js

---
+++ triggers/dropdown.js
@@ -0,0 +1,42 @@
+const surveysdropdown = (z, bundle) => {
+const customHttpOptions = {
+    headers: {
+      'content-type': 'application/json',
+      'X-API-TOKEN': bundle.inputData.apikey
+    }
+  };
+return z
+    .request('https://qualtrics.com/API/v3/surveys', customHttpOptions)
+    .then(response => {
+      if (response.status >= 300) {
+        throw new Error(`Unexpected status code ${response.status}`);
+      } 
+     a = []
+     str = z.JSON.parse(response.content);
+     
+     str.result.elements.forEach(s => {
+         id = s.id;
+         name = s.name;
+         json = "{ id: " + id + ", name:" + name + " }"
+         z.console.log(json);
+         a.push(json) 
+    });
+    return a;
+    });
+};
+
+
+module.exports = {
+  key: 'dropdown',
+  noun: 'dropdown',
+  display: {
+    label: 'List Surveys',
+    description: 'List Surveys for this account'
+  },
+      operation: {
+        inputFields: [ 
+          {key: 'apikey', type: 'string',  helpText: 'what is your Qualtrics API Key?'},
+        ],
+        perform: surveysdropdown,
+},
+};
