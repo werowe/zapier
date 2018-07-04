731caa71ca724683b4bdeda92475520c4a6e85ea
werowe <werowe@walkerrowe.com>
2018-07-04 12:43:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
 const surveys = require('./triggers/surveys');
 const dropdown = require('./triggers/dropdown');
 
-// We can roll up all our behaviors in an App.
+
+const authentication = {
+  type: 'custom',
+  connectionLabel: '{{bundle.authData.apikey}}',
+  test: {
+    url: 'https://example.com/api/accounts/me.json'
+  },
+  fields: [
+    {
+      key: 'apikey',
+      type: 'string',
+      required: true,
+      helpText: 'This is your Qualtrics API Key'
+    }
+  ]
+};
+
 const App = {
-  // This is just shorthand to reference the installed dependencies you have. Zapier will
-  // need to know these before we can upload
   version: require('./package.json').version,
   platformVersion: require('zapier-platform-core').version,
-
-  // beforeRequest & afterResponse are optional hooks into the provided HTTP client
-  beforeRequest: [
-  ],
 
   afterResponse: [
   ],
 
-  // If you want to define optional resources to simplify creation of triggers, searches, creates - do that here!
   resources: {
   },
 
-  // If you want your trigger to show up, you better include it here!
   triggers: {
      [dropdown.key]: dropdown,
      [surveys.key]: surveys
   },
 
-  // If you want your searches to show up, you better include it here!
   searches: {
   },
 
-  // If you want your creates to show up, you better include it here!
+authentication: authentication,
+
+beforeRequest: [],
+
   creates: {
   }
 };
